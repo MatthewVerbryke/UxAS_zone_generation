1d5d9ae961ff57f2dcb5f775a712817990a4abc8
Matthew Verbryke <verbrymr@mail.uc.edu>
2017-09-28 17:19:51 -0400

    Split off functions from png_to_polygons.py that are applied to one zone at a time, and made them methods of a Zone object.

--- uxaszones/zones.py
+++ uxaszones/zones.py
@@ -4,7 +4,8 @@
 # Additional copyright may be held by others, as reflected in the commit history.
 
 
-import numpy as np
+#import numpy as np
+from shapely.geometry import Polygon, MultiPoint
 
 
 class Zone(object):
@@ -23,9 +24,8 @@ class Zone(object):
                         1 - Entry (characteristic)
                         2 - No Entry
                         
-        bound --        list of boundary points (list of float 2-tuples)  
+        bound --        zone boundary points (Shapely 'Polygon' object)  
         alt_range --    zone altitude range, min to max ([float, float])
-        buff --         required buffer size for keep-out zones (float)
 
     """
     
@@ -51,6 +51,61 @@ class Zone(object):
         # Determine altitude range
         self.alt_range = [uav_alt, uav_alt + 30.0] #<--NOTE: the value 30.0 is arbitrary for now
         
+    def buffer(self, uav_buffer):
+        """Add a buffer to the zone, equal to the relevant UAV's minimum turn radius using the Shapely buffer method."""
+
+        # Add buffer to the zone
+        buffed_bound = self.bound.buffer(uav_buffer)
+        
+        # Update the bound to the buffered boundary
+        self.bound = buffed_bound
+                
+    def crop(self, ao_bounds):
+        """'Crop' the zone to the boundaries of the AO using the Shapely intersection method."""
+
+        # Crop away boundary points outside the AO
+        cropped_bound = self.bound.intersection(ao_bounds)
+        
+        # Update the bound to the cropped boundary
+        self.bound = cropped_bound
+        
+    def make_convex(self):
+        """Create a convex zone around the current zone using the Shapely convex_hull method"""
+        
+        # Put boundary points into a Shapely MultiPoint Object
+        bound_points_list = list(self.bound.exterior.coords)
+        bound_multipoint = MultiPoint(bound_points_list)
+        
+        # Create convex-hull around all the bound points
+        convex_bound = bound_multipoint.convex_hull
+        
+        # Update the bound to the convex boundary
+        self.bound = convex_bound
+        
+    def reduce_vertices(self):
+        """Reduce the number of boundary points in the zone using the Shapely simplify method."""
+        
+        # Simplification factor
+        simple_factor = 0.0
+        
+        # Put boundary points into a list
+        bound_point_list = list(self.bound.exterior.coords)
+        
+        # while there are more than 64 points in the boundary point list
+        while (len(boundary_point_list) >= Zone.max_verts):
+            
+            # Increment simplification factor
+            simple_factor += 0.01
+            
+            # Simplify the boundary points using the new simplification factor
+            simple_bound = self.bound.simplify(simple_factor)
+            
+            # Put simplified boundary points into a list
+            bound_point_list = list(simp_bound.exterior.coords)
+        
+        # Update the bound to the simple boundary
+        self.bound = simple_bound
+            
     def display_info(self):
         """Output zone information to the terminal."""
         
@@ -59,11 +114,11 @@ class Zone(object):
         # Determine characteristics output
         for i in range(0,4):
             if (self.ztype[i] == 0):
-                zoutput[i] = "No"
+                zoutput[i] = "None"
             elif (self.ztype[i] == 1):
-                zoutput[i] = "Yes, permissive"
+                zoutput[i] = "Permissive"
             else:
-                zoutput[i] = "Yes, restrictive"
+                zoutput[i] = "Restrictive"
                 
         # Find if UAVs are permitted to enter the zone
         if 2 in self.ztype:
